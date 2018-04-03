bc111ebb56812b5b9923b58c8e9003c76f61cc38
Shuo Peng <pengshuo2014@gmail.com>
2018-04-03 09:35:24 -0700

    add ajax

--- controllers/sportsStore.js
+++ controllers/sportsStore.js
@@ -1,15 +1,13 @@
 angular.module("sportsStore")
-			 .controller("sportsStoreCtrl", function($scope) {
+			 .constant("dataUrl", "http://localhost:5500/products")
+			 .controller("sportsStoreCtrl", function($scope, $http, dataUrl) {
 
-			 	$scope.data = {
-					products: [
-						{ name: "Product #2", description: "A product",
-							category: "category #2", price: 100},
-						{ name: "Product #3", description: "A product",
-							category: "category #3", price: 110},
-						{ name: "Product #1", description: "A product",
-							category: "category #1", price: 120},
-						{ name: "Product #4", description: "A product",
-							category: "category #1", price: 130}]
-			 	};
+			 	$scope.data = {};
+
+			 	$http.get(dataUrl)
+			 			 .then(function(data){
+			 			 	$scope.data.products = data.data;
+			 			 }, function(error){
+			 			 	$scope.data.error = error;
+			 			 });
 			 });
